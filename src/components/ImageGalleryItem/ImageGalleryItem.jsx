6c2bea904cf24eb23e56c3bfb83e9c593f19ff8d
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-10-21 21:11:58 +0200

    fixed ImageGalleryItem

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,37 +1,64 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { ModalEl } from '../Modal/Modal.jsx';
 import { ImgGallery, ItemGallery } from './ImageGalleryItem.styled.js';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    isModalOpen: false,
+export const ImageGalleryItem = props => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { dataForCard } = props;
+
+  const openModal = () => {
+    setIsModalOpen(true);
   };
 
-  openModal = () => {
-    this.setState({ isModalOpen: true });
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
+  return (
+    <ItemGallery>
+      <ImgGallery
+        onClick={openModal}
+        src={dataForCard.webformatURL}
+      ></ImgGallery>
 
-  closeModal = () => {
-    this.setState({ isModalOpen: false });
-  };
+      <ModalEl
+        data={dataForCard}
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+      ></ModalEl>
+    </ItemGallery>
+  );
+};
 
-  render() {
-    const { isModalOpen } = this.state;
-    const { dataForCard } = this.props;
+// export class ImageGalleryItem1 extends Component {
+//   state = {
+//     isModalOpen: false,
+//   };
 
-    return (
-      <ItemGallery>
-        <ImgGallery
-          onClick={this.openModal}
-          src={dataForCard.webformatURL}
-        ></ImgGallery>
+//   openModal = () => {
+//     this.setState({ isModalOpen: true });
+//   };
 
-        <ModalEl
-          data={dataForCard}
-          isOpen={isModalOpen}
-          onRequestClose={this.closeModal}
-        ></ModalEl>
-      </ItemGallery>
-    );
-  }
-}
+//   closeModal = () => {
+//     this.setState({ isModalOpen: false });
+//   };
+
+//   render() {
+//     const { isModalOpen } = this.state;
+//     const { dataForCard } = this.props;
+
+//     return (
+//       <ItemGallery>
+//         <ImgGallery
+//           onClick={this.openModal}
+//           src={dataForCard.webformatURL}
+//         ></ImgGallery>
+
+//         <ModalEl
+//           data={dataForCard}
+//           isOpen={isModalOpen}
+//           onRequestClose={this.closeModal}
+//         ></ModalEl>
+//       </ItemGallery>
+//     );
+//   }
+// }
